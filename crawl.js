2aef38ebe6aeb7b2bd12baa2d93a1ad59f4bce84
albert <kipchirchiralb@gmail.com>
2023-05-03 21:26:02 +0300

    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(urlstring) {
+  const urlObj = new URL(urlstring);
+  console.log(urlObj);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+// normalizeURL("https://blog.boot.dev/path");
+
+module.exports = { normalizeURL };
